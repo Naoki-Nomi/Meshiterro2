493acadd3d352b83210b83a0e68f941959e25384
Naoki-Nomi <ec2-user@ip-172-31-34-154.ec2.internal>
2021-01-17 05:12:43 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '703dd012edcb01f7e49bd14c87db270cc2fac990a4953d3baab55eb1aa39687e9e33f0d39d1b687473a4bb07c56a0a8fdc293567d0020eda1361a3579a311b2c'
